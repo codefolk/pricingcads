e3351fc921c254ee221b9fd5d52b9af2040d8633
codefolk <94558069+codefolk@users.noreply.github.com>
2022-08-26 23:04:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+document.querySelector(".toggle").onclick = function(){
 
-    document.querySelector(".toggle").addEventListener("click", clickMe); 
-
-function clickMe() {
+if (document.querySelector(".toggle").checked == true){
   var price = document.querySelector("#pricing");
   price.textContent = "$199.99";
   var price2 = document.querySelector("#pricing2");
   price2.textContent = "$249.99";
   var price3 = document.querySelector("#pricing3");
   price3.textContent = "$399.99";
+}
+else{
+  var price = document.querySelector("#pricing");
+  price.textContent = "$19.99";
+  var price2 = document.querySelector("#pricing2");
+  price2.textContent = "$24.99";
+  var price3 = document.querySelector("#pricing3");
+  price3.textContent = "$39.99";
+}
 }
 
