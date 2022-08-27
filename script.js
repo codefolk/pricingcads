042a279211e65eca180f13cf4e0dfeace0be89c3
codefolk <alizuhaib828@gmail.com>
2022-08-27 22:13:40 +0500

    changed the variable scope in JS file

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 document.querySelector(".toggle").onclick = function(){
 
 if (document.querySelector(".toggle").checked == true){
-  var price = document.querySelector("#pricing");
+  let price = document.querySelector("#pricing");
   price.textContent = "$199.99";
-  var price2 = document.querySelector("#pricing2");
+  let price2 = document.querySelector("#pricing2");
   price2.textContent = "$249.99";
-  var price3 = document.querySelector("#pricing3");
+  let price3 = document.querySelector("#pricing3");
   price3.textContent = "$399.99";
 }
 else{
-  var price = document.querySelector("#pricing");
+  let price = document.querySelector("#pricing");
   price.textContent = "$19.99";
-  var price2 = document.querySelector("#pricing2");
+  let price2 = document.querySelector("#pricing2");
   price2.textContent = "$24.99";
-  var price3 = document.querySelector("#pricing3");
+  let price3 = document.querySelector("#pricing3");
   price3.textContent = "$39.99";
 }
 }
-
